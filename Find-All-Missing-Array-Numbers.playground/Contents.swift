8366b34372a5d65d3397a7d667caa23603f64e09
Herun Zhou <iamherunzhou@gmail.com>
2019-09-19 23:26:00 -0400

    experiment code

---
+++ Find-All-Missing-Array-Numbers.playground/Contents.swift
@@ -0,0 +1,5 @@
+let array = [Int]()
+
+let length = array.count
+
+print(length)
---
+++ Unique Paths.playground/Contents.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+var str = "Hello, playground"
+var arr = [[Int]](repeating: [Int](repeating: 0, count: 3), count: 2)
+
+
