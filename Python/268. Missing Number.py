46195ff99bef46b8afc651d639c62b0d1ca5af86
Herun Zhou <iamherunzhou@gmail.com>
2020-05-06 00:27:29 -0400

    finished lc 268

---
+++ Python/268. Missing Number.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        total = (n * (n + 1)) / 2
+        for i in nums:
+            total -= i
+
+        return total
