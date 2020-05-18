0b3595e45557ec8e77f12d2fb62ee37f28d8c2e6
Herun Zhou <iamherunzhou@gmail.com>
2020-05-18 15:16:53 -0400

    finished lc 98 - added iterative DFS solution

--- Python/Medium/98. Validate Binary Search Tree.py
+++ Python/Medium/98. Validate Binary Search Tree.py
@@ -26,3 +26,38 @@ class Solution(object):
             return True
 
         return helper(root, float('-inf'), float('inf'))
+
+
+# Iterative Solution
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:
+            return True
+
+        stack = []
+        stack.append((root, (float('-inf'), float('inf'))))
+        while stack:
+            item = stack.pop()
+            node = item[0]
+            left = item[1][0]
+            right = item[1][1]
+
+            if node is None:
+                continue
+            elif node.val <= left or node.val >= right:
+                return False
+            else:
+                stack.append((node.left, (left, node.val)))
+                stack.append((node.right, (node.val, right)))
+
+        return True
