795dac12ba5d16a97a19c0e6856f81290435ed06
Herun Zhou <iamherunzhou@gmail.com>
2020-05-24 15:17:15 -0400

    redid subsets lc 78

--- Python/Medium/78. Subsets.py
+++ Python/Medium/78. Subsets.py
@@ -1,18 +1,18 @@
 class Solution(object):
-    def subsets(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        results = []
-
-        def backtrack(tempList, start):
-            results.append(tempList)
-            for i in range(start, len(nums)):
-                tempList.append(nums[i])
-                backtrack(tempList[:], i + 1)
-                tempList.pop()
-
-        nums.sort()
-        backtrack([], 0)
-        return results
+def subsets(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: List[List[int]]
+    """
+    answer = []
+    length = len(nums)
+    
+    def backtrack(currArr, currIndex):
+        answer.append(currArr)
+        for i in range(currIndex, length):
+            currArr.append(nums[i])
+            backtrack(currArr[:], i + 1)
+            currArr.pop()
+    
+    backtrack([], 0)
+    return answer
