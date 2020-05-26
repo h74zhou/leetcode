0f94ca3d7a8e854ba68720965f5fc10626fe76eb
Herun Zhou <iamherunzhou@gmail.com>
2020-05-25 21:40:27 -0400

    finished lc 39

--- Python/Medium/39. Combination Sum.py
+++ Python/Medium/39. Combination Sum.py
@@ -1,1 +1,25 @@
-import sys
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        candidates.sort()
+        answer = []
+
+        def backtrack(currArr, currSum, start):
+            if currSum == target:
+                answer.append(currArr)
+            else:
+                for i in range(start, len(candidates)):
+                    if currSum + candidates[i] > target:
+                        break
+                    currArr.append(candidates[i])
+                    currSum += candidates[i]
+                    backtrack(currArr[:], currSum, i)
+                    currArr.pop()
+                    currSum -= candidates[i]
+
+        backtrack([], 0, 0)
+        return answer
