5d37833fe9c57d4436d61270e46d421285d71c79
Herun Zhou <iamherunzhou@gmail.com>
2020-06-08 10:07:22 -0400

    finished lc 62

---
+++ Python/Medium/62. Unique Paths.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        dp = [[0 for x in range(n)] for y in range(m)]
+        dp[0][0] = 1
+
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j == 0:
+                    continue
+                elif i == 0:
+                    dp[i][j] = dp[i][j - 1]
+                elif j == 0:
+                    dp[i][j] = dp[i - 1][j]
+                else:
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+
+        return dp[i][j]
