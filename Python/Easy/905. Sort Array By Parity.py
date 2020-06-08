3d05d67d8582708c933aad665a0806401769ae3d
Herun Zhou <iamherunzhou@gmail.com>
2020-06-08 10:30:00 -0400

    finished lc 905 - in place

--- Python/Easy/905. Sort Array By Parity.py
+++ Python/Easy/905. Sort Array By Parity.py
@@ -4,12 +4,27 @@ class Solution(object):
         :type A: List[int]
         :rtype: List[int]
         """
-        even = []
-        odd = []
-        for num in A:
-            if num % 2 == 0:
-                even.append(num)
-            else:
-                odd.append(num)
+        if len(A) == 0:
+            return A
 
-        return even + odd
+        i = 0
+        j = len(A) - 1
+
+        while i < j:
+            if A[i] % 2 != 0 and A[j] % 2 == 0:
+                # i is odd (1), j is even (0)
+                A[i], A[j] = A[j], A[i]
+                i += 1
+                j -= 1
+            elif A[i] % 2 == 0:
+                # i is even (0), move i up
+                i += 1
+            elif A[j] % 2 == 0 and A[i] % 2 != 0:
+                # if j is even and i is not even, swap
+                A[i], A[j] = A[j], A[i]
+                i += 1
+                j -= 1
+            elif A[j] % 2 != 0 and A[i] % 2 != 0:
+                j -= 1
+
+        return A
