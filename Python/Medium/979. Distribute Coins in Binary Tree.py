4c19fc8efad5ea7a4003d1dfbf1a593ab1350454
Herun Zhou <iamherunzhou@gmail.com>
2020-06-12 10:24:41 -0400

    finished lc 979

---
+++ Python/Medium/979. Distribute Coins in Binary Tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def distributeCoins(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        self.answer = 0
+
+        def dfs(root):
+            if not root:
+                return 0
+            left = dfs(root.left)
+            right = dfs(root.right)
+            self.answer = self.answer + abs(left) + abs(right)
+            return root.val + left + right - 1
+
+        dfs(root)
+        return self.answer
