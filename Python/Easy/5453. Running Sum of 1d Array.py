122f15d18bc7e463df540865b0e27cb2045998b6
Herun Zhou <iamherunzhou@gmail.com>
2020-06-14 11:57:15 -0400

    redid lc 5453 with o(1) space

--- Python/Easy/5453. Running Sum of 1d Array.py
+++ Python/Easy/5453. Running Sum of 1d Array.py
@@ -6,7 +6,6 @@ class Solution(object):
         """
         if len(nums) == 0:
             return 0
-        answer = [nums[0]]
         for i in range(1, len(nums)):
-            answer.append(nums[i] + answer[i - 1])
-        return answer
+            nums[i] = nums[i - 1] + nums[i]
+        return nums
