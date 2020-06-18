9a4e7df28dae00d137613508f216323e0e75ef97
Herun Zhou <iamherunzhou@gmail.com>
2020-06-18 12:07:36 -0400

    finished lc 1460

---
+++ Python/Easy/1460. Make Two Arrays Equal by Reversing Sub-arrays.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def canBeEqual(self, target, arr):
+        """
+        :type target: List[int]
+        :type arr: List[int]
+        :rtype: bool
+        """
+        target.sort()
+        arr.sort()
+        return target == arr
