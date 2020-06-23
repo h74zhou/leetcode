d04d38c74a2e625495d9b0019b07cb3c3735b276
Herun Zhou <iamherunzhou@gmail.com>
2020-06-23 12:05:10 -0400

    redid lc 15

--- Medium/15. 3Sum.py
+++ Medium/15. 3Sum.py
@@ -5,22 +5,12 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         nums.sort()
-        i = 0
-        k = len(nums) - 1
-
-        if len(nums) < 3:
-            return []
-
         answer = []
-
-        # N iterations
-        for i in range(0, len(nums)):
-            # Bounded by N
-            if i != 0 and nums[i] == nums[i - 1]:
-                continue
+        for i in range(len(nums)):
             j = i + 1
             k = len(nums) - 1
-            # Bounded by N
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
             while j < k:
                 if nums[i] + nums[j] + nums[k] == 0:
                     answer.append([nums[i], nums[j], nums[k]])
@@ -29,7 +19,7 @@ class Solution(object):
                         j += 1
                 elif nums[i] + nums[j] + nums[k] < 0:
                     j += 1
-                else:
+                elif nums[i] + nums[j] + nums[k] > 0:
                     k -= 1
 
         return answer
