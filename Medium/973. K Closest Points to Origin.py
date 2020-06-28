fae19f516337c5707fa941cb1bdd410ad68b322d
Herun Zhou <iamherunzhou@gmail.com>
2020-06-28 18:02:52 -0400

    redid lc 973 with O(nlogk) time

--- Medium/973. K Closest Points to Origin.py
+++ Medium/973. K Closest Points to Origin.py
@@ -10,14 +10,17 @@ class Solution(object):
         """
         def distance(point):
             xPoint, yPoint = point
-            return sqrt((xPoint * xPoint) + (yPoint * yPoint))
+            return 0 - sqrt((xPoint * xPoint) + (yPoint * yPoint))
 
         li = []
         for point in points:
-            heapq.heappush(li, (distance(point), point))
+            if len(li) < K:
+                heapq.heappush(li, (distance(point), point))
+            elif distance(point) < li[0]:
+                heapq.heappushpop(li, (distance(point), point))
 
         answer = []
-        for i in range(K):
-            answer.append(heapq.heappop(li)[1])
+        for point in li:
+            answer.append(point[1])
 
         return answer
