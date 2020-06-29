dfd23a72e65f729bb1056c4403f173c85d1d1cf8
Herun Zhou <iamherunzhou@gmail.com>
2020-06-28 20:43:22 -0400

    redid lc 17

--- Medium/17. Letter Combinations of a Phone Number.py
+++ Medium/17. Letter Combinations of a Phone Number.py
@@ -4,23 +4,29 @@ class Solution(object):
         :type digits: str
         :rtype: List[str]
         """
-        answer = []
-        phone = {'2': ['a', 'b', 'c'],
-                 '3': ['d', 'e', 'f'],
-                 '4': ['g', 'h', 'i'],
-                 '5': ['j', 'k', 'l'],
-                 '6': ['m', 'n', 'o'],
-                 '7': ['p', 'q', 'r', 's'],
-                 '8': ['t', 'u', 'v'],
-                 '9': ['w', 'x', 'y', 'z']}
+        d = {
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y", "z"]
+        }
 
-        def backtrack(currArr, currIndex):
-            if len(currArr) == len(digits):
-                if len(currArr) != 0:
-                    answer.append(currArr)
+        answer = []
+
+        def backtrack(currStr, currIndex):
+            if len(currStr) == len(digits):
+                answer.append(currStr[:])
             else:
-                for letter in phone[digits[currIndex]]:
-                    backtrack(currArr + letter, currIndex + 1)
+                for i in range(currIndex, len(digits)):
+                    for letter in d[digits[i]]:
+                        backtrack(currStr + letter, i + 1)
+
+        if len(digits) == 0:
+            return []
 
         backtrack("", 0)
         return answer
