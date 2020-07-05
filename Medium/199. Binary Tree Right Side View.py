0b83951011a18f21620bc3f38f08a7224ba286e0
Herun Zhou <iamherunzhou@gmail.com>
2020-07-05 19:29:48 -0400

    finished lc 199

---
+++ Medium/199. Binary Tree Right Side View.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def rightSideView(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return root
+
+        answer = []
+        queue = [root]
+
+        while queue:
+            answer.append(queue[-1].val)
+            temp = []
+            for node in queue:
+                if node.left:
+                    temp.append(node.left)
+                if node.right:
+                    temp.append(node.right)
+            queue = temp[:]
+
+        return answer
