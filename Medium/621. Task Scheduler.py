c8ae7ab3e0a69009a67b6433235992f1fd865598
Herun Zhou <iamherunzhou@gmail.com>
2020-07-14 14:38:30 -0400

    finished lc 621

--- Medium/621. Task Scheduler.py
+++ Medium/621. Task Scheduler.py
@@ -6,19 +6,17 @@ class Solution(object):
         :rtype: int
         """
         d = {}
-        mostFreq = float('-inf')
+        mode = 0
         for task in tasks:
             if task in d:
                 d[task] += 1
             else:
                 d[task] = 1
-            if d[task] >= mostFreq:
-                mostFreq = d[task]
+            mode = max(mode, d[task])
 
-        answer = (mostFreq - 1) * (n + 1)
+        count = (mode - 1) * (n + 1)
+        for index, value in d.iteritems():
+            if value == mode:
+                count += 1
 
-        for task, freq in d.iteritems():
-            if freq == mostFreq:
-                answer += 1
-
-        return max(len(tasks), answer)
+        return max(count, len(tasks))
