d95b8d55a022e5628e9e96c5437a0729f0978979
Herun Zhou <iamherunzhou@gmail.com>
2020-07-14 14:05:04 -0400

    redid lc 91

--- Medium/91. Decode Ways.py
+++ Medium/91. Decode Ways.py
@@ -4,20 +4,17 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        dp = [0] * (len(s) + 1)
+        if s[0] == '0':
+            return 0
+
+        dp = [0 for x in range(len(s) + 1)]
         dp[0] = 1
-        dp[1] = 1 if int(s[0]) >= 1 else 0
+        dp[1] = 1
 
         for i in range(2, len(s) + 1):
-            first = int(s[i - 1: i])
-            second = int(s[i - 2: i])
-
-            if first >= 1:
+            if 0 < int(s[i - 1: i]) <= 9:
                 dp[i] += dp[i - 1]
-
-            if second >= 10 and second <= 26:
+            if 10 <= int(s[i - 2: i]) <= 26:
                 dp[i] += dp[i - 2]
 
         return dp[-1]
-
-
