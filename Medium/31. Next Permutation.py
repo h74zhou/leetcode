1828341c6965a5d6f3912134a3c44ebd27d9ec83
Herun Zhou <iamherunzhou@gmail.com>
2020-07-17 16:43:29 -0400

    redid lc 31

--- Medium/31. Next Permutation.py
+++ Medium/31. Next Permutation.py
@@ -4,18 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-
-        start = float('-inf')
+        k = len(nums) - 1
         for i in range(len(nums) - 2, -1, -1):
             if nums[i] < nums[i + 1]:
-                start = i
+                k = i
                 break
 
-        if start == float('-inf'):
+        if k == len(nums) - 1:
             nums.reverse()
-        else:
-            for i in range(len(nums) - 1, -1, -1):
-                if nums[i] > nums[start]:
-                    nums[i], nums[start] = nums[start], nums[i]
-                    nums[start + 1: len(nums)] = nums[len(nums) - 1: start: -1]
-                    break
+            return
+
+        for i in range(len(nums) - 1, -1, -1):
+            if i > k and nums[i] > nums[k]:
+                nums[i], nums[k] = nums[k], nums[i]
+                nums[k + 1: len(nums)] = nums[len(nums) - 1: k: -1]
+                break
