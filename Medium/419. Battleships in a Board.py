c341d3c7b3616c528cdf78b10ab7128adccd7e14
Herun Zhou <iamherunzhou@gmail.com>
2020-07-21 01:28:03 -0400

    finished lc 419

---
+++ Medium/419. Battleships in a Board.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def countBattleships(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: int
+        """
+        count = 0
+        for y in range(len(board)):
+            for x in range(len(board[0])):
+                if (board[y][x] == 'X' and (y == 0 or board[y - 1][x] != 'X') and (x == 0 or board[y][x - 1] != 'X')):
+                    count += 1
+        return count
