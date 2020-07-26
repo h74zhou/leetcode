9883061b3443719289cd537293a23a98ff29131f
Herun Zhou <iamherunzhou@gmail.com>
2020-07-25 23:34:00 -0400

    finished lc 513

---
+++ Medium/513. Find Bottom Left Tree Value.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def findBottomLeftValue(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        queue = [root]
+        curr = root
+        while queue:
+            curr = queue[0]
+            temp = []
+            for node in queue:
+                if node.left:
+                    temp.append(node.left)
+                if node.right:
+                    temp.append(node.right)
+            queue = temp[:]
+        return curr.val
