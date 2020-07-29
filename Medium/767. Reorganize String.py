2f54e3ed5853b87387c80e6057fa1c9cdb572951
Herun Zhou <iamherunzhou@gmail.com>
2020-07-28 23:23:54 -0400

    started lc 767

---
+++ Medium/767. Reorganize String.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def reorganizeString(self, S):
+        """
+        :type S: str
+        :rtype: str
+        """
+        d = {}
+        mFreq = 0
+        for c in S:
+            if c in d:
+                d[c] += 1
+            else:
+                d[c] = 1
+            if d[c] > mFreq:
+                mFreq = d[c]
