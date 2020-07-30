e98b7cf8ae9f1d12ebf60131e654182a9f80a86b
Herun Zhou <iamherunzhou@gmail.com>
2020-07-30 01:18:58 -0400

    finished lc 767

--- Medium/767. Reorganize String.py
+++ Medium/767. Reorganize String.py
@@ -4,14 +4,29 @@ class Solution(object):
         :type S: str
         :rtype: str
         """
-
-        # use heap
-        d = {}
-        mFreq = 0
+        hash = [0 for x in range(26)]
+        mostFreqLetter, mostFreq = "", 0
         for c in S:
-            if c in d:
-                d[c] += 1
-            else:
-                d[c] = 1
-            if d[c] > mFreq:
-                mFreq = d[c]
+            hash[ord(c) - 97] += 1
+            if hash[ord(c) - 97] > mostFreq:
+                mostFreq = hash[ord(c) - 97]
+                mostFreqLetter = c
+
+        if mostFreq > (len(S) + 1) / 2:
+            return ""
+        index, ansArr = 0, ["?" for x in range(len(S))]
+
+        while hash[ord(mostFreqLetter) - 97] > 0:
+            ansArr[index] = mostFreqLetter
+            hash[ord(mostFreqLetter) - 97] -= 1
+            index += 2
+
+        for i in range(len(hash)):
+            while hash[i] > 0:
+                if index >= len(S):
+                    index = 1
+                ansArr[index] = chr(i + 97)
+                index += 2
+                hash[i] -= 1
+
+        return ''.join(ansArr)
