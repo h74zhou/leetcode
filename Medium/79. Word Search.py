b69676556a10d2102a99101475063ccb7dd6e9c9
Herun Zhou <iamherunzhou@gmail.com>
2020-08-05 09:49:54 -0400

    finished lc 79

--- Medium/79. Word Search.py
+++ Medium/79. Word Search.py
@@ -5,44 +5,26 @@ class Solution(object):
         :type word: str
         :rtype: bool
         """
-        # dfs approach
-        map = {}
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                letter = board[i][j]
-                if letter in map:
-                    map[letter].append((i, j))
-                else:
-                    map[letter] = [(i, j)]
-
-        def backtrack(currWord, currIndex, row, col, used):
-            print "current hashmap"
-            print used
-            print "current index"
-            print(row, col)
+        def dfs(currBoard, currRow, currColumn, currWord, currIndex):
             if currWord == word:
                 return True
-            else:
-                if row + 1 < len(board) and board[row + 1][col] == word[currIndex] and (row + 1, col) not in used:
-                    used[(row, col)] = 1
-                    return backtrack(currWord + word[currIndex], currIndex + 1, row + 1, col, used.copy())
-                if row - 1 >= 0 and board[row - 1][col] == word[currIndex] and (row - 1, col) not in used:
-                    used[(row, col)] = 1
-                    return backtrack(currWord + word[currIndex], currIndex + 1, row - 1, col, used.copy())
-                if col + 1 < len(board[0]) and board[row][col + 1] == word[currIndex] and (row, col + 1) not in used:
-                    used[(row, col)] = 1
-                    return backtrack(currWord + word[currIndex], currIndex + 1, row, col + 1, used.copy())
-                if col - 1 >= 0 and board[row][col - 1] == word[currIndex] and (row, col - 1) not in used:
-                    used[(row, col)] = 1
-                    return backtrack(currWord + word[currIndex], currIndex + 1, row, col - 1, used.copy())
 
-        if word[0] not in map:
-            return False
-        indexArr = map[word[0]]
+            if len(currWord) >= len(word):
+                return False
 
-        for row, col in indexArr:
-            print "new starting point"
-            print(row, col)
-            if backtrack(word[0], 1, row, col, {}) == True:
-                return True
+            if currRow < 0 or currRow >= len(board) or currColumn < 0 or currColumn >= len(board[0]) or board[currRow][currColumn] != word[currIndex]:
+                return False
+
+            temp = board[currRow][currColumn]
+            currBoard[currRow][currColumn] = ' '
+
+            canContinue = dfs(currBoard[:], currRow - 1, currColumn, currWord + temp, currIndex + 1) or dfs(currBoard[:], currRow + 1, currColumn, currWord + temp, currIndex + 1) or dfs(currBoard[:], currRow, currColumn - 1, currWord + temp, currIndex + 1) or dfs(currBoard[:], currRow, currColumn + 1, currWord + temp, currIndex + 1)
+
+            currBoard[currRow][currColumn] = temp
+            return canContinue
+
+        for row in range(len(board)):
+            for column in range(len(board[0])):
+                if board[row][column] == word[0] and dfs(board, row, column, "", 0):
+                    return True
         return False
