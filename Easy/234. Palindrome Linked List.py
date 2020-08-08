2f7eb5cb70977f07cf42807407d66f1e477d4d7b
Herun Zhou <iamherunzhou@gmail.com>
2020-08-08 19:31:21 -0400

    redid lc 234

--- Easy/234. Palindrome Linked List.py
+++ Easy/234. Palindrome Linked List.py
@@ -1,46 +1,37 @@
 # Definition for singly-linked list.
 # class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
-
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution(object):
-
-    def reverseLst(self, root):
-        curr = root
-        prev = None
-
-        while curr is not None:
-            next = curr.next
-            curr.next = prev
-            prev = curr
-            curr = next
-
-        return prev
-
     def isPalindrome(self, head):
         """
         :type head: ListNode
         :rtype: bool
         """
-        if head is None:
-            return True
-
         slow = head
         fast = head
 
-        while fast is not None and fast.next is not None:
+        while fast and fast.next:
+            slow = slow.next
             fast = fast.next.next
+
+        if fast:
             slow = slow.next
 
-        tail = self.reverseLst(slow)
+        prev = None
+        while slow:
+            next = slow.next
+            slow.next = prev
+            prev = slow
+            slow = next
 
-        while head is not None and tail is not None:
-            if head.val != tail.val:
+        start, end = head, prev
+
+        while end:
+            if start.val != end.val:
                 return False
-
-            head = head.next
-            tail = tail.next
+            start = start.next
+            end = end.next
 
         return True
