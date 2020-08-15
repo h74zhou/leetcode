70e384731c9c078b070df7b276aa299514f28c87
Herun Zhou <iamherunzhou@gmail.com>
2020-08-14 22:51:58 -0400

    redid lc 278 with binary search template

--- Easy/278. First Bad Version.py
+++ Easy/278. First Bad Version.py
@@ -9,14 +9,11 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        low = 1
-        high = n
-
-        while low <= high:
-            mid = (low + high) / 2
+        left, right = 1, n
+        while left < right:
+            mid = left + (right - left) // 2
             if isBadVersion(mid):
-                high = mid - 1
+                right = mid
             else:
-                low = mid + 1
-
-        return low
+                left = mid + 1
+        return left
