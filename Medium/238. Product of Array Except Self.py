923656e665a78c2a0eff91a3770c9e3ca38a5398
Herun Zhou <iamherunzhou@gmail.com>
2020-09-06 15:13:03 -0400

    redid lc 238

--- Medium/238. Product of Array Except Self.py
+++ Medium/238. Product of Array Except Self.py
@@ -4,15 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        leftArray = []
-        leftArray.append(1)
-
+        leftSum = [1]
         for i in range(1, len(nums)):
-            leftArray.append(leftArray[i - 1] * nums[i - 1])
+            leftSum.append(leftSum[i - 1] * nums[i - 1])
 
-        right = 1
+        rightSum = 1
         for i in range(len(nums) - 1, -1, -1):
-            leftArray[i] *= right
-            right *= nums[i]
+            leftSum[i] *= rightSum
+            rightSum *= nums[i]
 
-        return leftArray
+        return leftSum
