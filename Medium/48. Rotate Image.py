875149e32968c5bf78f79e4f3ec74b4c59b84bbd
Herun Zhou <iamherunzhou@gmail.com>
2020-09-06 23:02:39 -0400

    redid lc 48

--- Medium/48. Rotate Image.py
+++ Medium/48. Rotate Image.py
@@ -4,9 +4,16 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
-        # Reverse up and down
-        matrix.reverse()
-        # Swap the symmetry
-        for row in range(len(matrix)):
-            for column in range(row + 1, len(matrix[0])):
-                matrix[row][column], matrix[column][row] = matrix[column][row], matrix[row][column]
+
+        # reverse top-down
+        i, j = 0, len(matrix) - 1
+        while i < j:
+            matrix[i], matrix[j] = matrix[j], matrix[i]
+            i += 1
+            j -= 1
+
+        # swap based on x and y
+        for x in range(len(matrix)):
+            for y in range(x, len(matrix[0])):
+                print x, y
+                matrix[x][y], matrix[y][x] = matrix[y][x], matrix[x][y]
