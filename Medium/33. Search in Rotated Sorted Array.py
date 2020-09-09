39bdba63668a70e53cdf8d6227ee5834d92bc089
Herun Zhou <iamherunzhou@gmail.com>
2020-09-08 23:36:31 -0400

    redid lc 33 with 2 binary searches

--- Medium/33. Search in Rotated Sorted Array.py
+++ Medium/33. Search in Rotated Sorted Array.py
@@ -5,27 +5,32 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        if len(nums) == 0:
-            return -1
+        # find smallest element at pivot
+        left, right = 0, len(nums) - 1
+        while left < right:
+            mid = left + (right - left) // 2
+            if nums[mid] < nums[right]:
+                right = mid
+            else:
+                left = mid + 1
 
-        low = 0
-        high = len(nums) - 1
+        # left will be the pivot
+        pivot = left
 
-        while low <= high:
-            mid = (low + high) / 2
+        if nums[left] <= target <= nums[len(nums) - 1]:
+            # binary search from pivot to end
+            left, right = pivot, len(nums) - 1
+        else:
+            # binary search from 0 to pivot
+            left, right = 0, pivot
+
+        while left <= right:
+            mid = left + (right - left) // 2
             if nums[mid] == target:
                 return mid
-
-            if nums[low] <= nums[mid]:
-                if nums[low] <= target <= nums[mid]:
-                    high = mid - 1
-                else:
-                    low = mid + 1
+            elif nums[mid] < target:
+                left = mid + 1
             else:
-                if nums[mid] <= target <= nums[high]:
-                    low = mid + 1
-                else:
-                    high = mid - 1
+                right = mid - 1
 
         return -1
-
