aa133f468e700cef6cf1edb41b65177c9787d101
Herun Zhou <iamherunzhou@gmail.com>
2020-09-24 23:32:37 -0400

    redid lc 67

--- Easy/67. Add Binary.py
+++ Easy/67. Add Binary.py
@@ -7,12 +7,13 @@ class Solution(object):
         """
         if len(a) == 0:
             return b
+
         if len(b) == 0:
             return a
 
-        if a[-1] == "0" and b[-1] == "0":
-            return self.addBinary(a[:-1], b[:-1]) + "0"
-        elif a[-1] == "1" and b[-1] == "1":
-            return self.addBinary(self.addBinary(a[:-1], b[:-1]), "1") + "0"
+        if a[-1] == '0' and b[-1] == '0':
+            return self.addBinary(a[:-1], b[:-1]) + '0'
+        elif a[-1] == '1' and b[-1] == '1':
+            return self.addBinary('1', self.addBinary(a[:-1], b[:-1])) + '0'
         else:
-            return self.addBinary(a[:-1], b[:-1]) + "1"
+            return self.addBinary(a[:-1], b[:-1]) + '1'
