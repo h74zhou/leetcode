44e0ccf26425aa8ea31efc3f5e329fd5494584b1
Herun Zhou <iamherunzhou@gmail.com>
2020-09-25 22:51:30 -0400

    redid lc 415

--- Easy/415. Add Strings.py
+++ Easy/415. Add Strings.py
@@ -7,10 +7,13 @@ class Solution(object):
         """
         if len(num1) == 0:
             return num2
+
         if len(num2) == 0:
             return num1
 
-        if int(num1[-1]) + int(num2[-1]) >= 10:
-            return self.addStrings(self.addStrings(num1[:-1], num2[:-1]), "1") + str(((int(num1[-1]) + int(num2[-1])) % 10))
-        elif int(num1[-1]) + int(num2[-1]) < 10:
-            return self.addStrings(num1[:-1], num2[:-1]) + str(int(num1[-1]) + int(num2[-1]))
+        int1, int2 = int(num1[-1]), int(num2[-1])
+        if int1 + int2 < 10:
+            return self.addStrings(num1[:-1], num2[:-1]) + str(int1 + int2)
+        else:
+            return self.addStrings(self.addStrings(num1[:-1], num2[:-1]), '1') + str(int1 + int2)[-1]
+
