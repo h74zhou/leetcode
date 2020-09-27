998d44c622ff615cfe0230d0f1d1be76e91f7f09
Herun Zhou <iamherunzhou@gmail.com>
2020-09-26 21:40:40 -0400

    redid lc 22

--- Medium/22. Generate Parentheses.py
+++ Medium/22. Generate Parentheses.py
@@ -6,15 +6,18 @@ class Solution(object):
         """
         answer = []
 
+        # left, right represent current left and right amount of brackets
         def backtrack(currStr, left, right):
             if left == n and right == n:
-                answer.append(currStr)
+                answer.append(currStr[:])
             else:
-                if left == n:
-                    backtrack(currStr + ")", left, right + 1)
-                elif left >= right:
-                    backtrack(currStr + "(", left + 1, right)
-                    backtrack(currStr + ")", left, right + 1)
+                if left == right:
+                    backtrack(currStr + '(', left + 1, right)
+                elif left > right and left < n:
+                    backtrack(currStr + '(', left + 1, right)
+                    backtrack(currStr + ')', left, right + 1)
+                elif left > right and left == n:
+                    backtrack(currStr + ')', left, right + 1)
 
         backtrack("", 0, 0)
         return answer
