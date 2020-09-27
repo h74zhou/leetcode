50f7f826e4305d8cc266af2247e80e61066ca87b
Herun Zhou <iamherunzhou@gmail.com>
2020-09-26 23:16:59 -0400

    redid lc 46

--- Medium/46. Permutations.py
+++ Medium/46. Permutations.py
@@ -5,23 +5,20 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         answer = []
-        length = len(nums)
-        
-        def backtrack(currArr, currRem, setArray):
-            if len(currArr) == length:
-                answer.append(currArr)
-            else:
-                for i in range(len(currRem)):
-                    if currRem[i] in setArray:
-                        continue
-                    currArr.append(currRem[i])
-                    setArray.add(currRem[i])
-                    tempRem = currRem[:]
-                    tempRem.remove(currRem[i])
-                    backtrack(currArr[:], tempRem[:], setArray.copy())
-                    currArr.pop()
-                    setArray.remove(currRem[i])
+        if len(nums) == 0:
+            return answer
 
-        arraySet = set()
-        backtrack([], nums[:], arraySet)
+        nums.sort()
+
+        def backtrack(currArr):
+            if len(currArr) >= len(nums):
+                answer.append(currArr[:])
+            else:
+                for i in range(len(nums)):
+                    if nums[i] in currArr:
+                        continue
+                    else:
+                        backtrack(currArr + [nums[i]])
+
+        backtrack([])
         return answer
