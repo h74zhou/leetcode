e7a1f1f21f0589b60ad5bf6ea40f4d06b6185d6b
Herun Zhou <iamherunzhou@gmail.com>
2020-09-27 23:59:07 -0400

    redid lc 131

--- Medium/131. Palindrome Partitioning.py
+++ Medium/131. Palindrome Partitioning.py
@@ -6,25 +6,22 @@ class Solution(object):
         """
         answer = []
 
-        def isPalindrome(string):
-            i = 0
-            j = len(string) - 1
-            while i < j:
-                if string[i] != string[j]:
+        def isPalindrome(word):
+            start, end = 0, len(word) - 1
+            while start <= end:
+                if word[start] != word[end]:
                     return False
-                i += 1
-                j -= 1
+                start += 1
+                end -= 1
             return True
 
         def backtrack(currArr, currIndex):
-            if currIndex == len(s):
-                answer.append(currArr)
+            if currIndex >= len(s):
+                answer.append(currArr[:])
             else:
                 for i in range(currIndex, len(s)):
-                    if isPalindrome(s[currIndex: i + 1]):
-                        currArr.append(s[currIndex: i + 1])
-                        backtrack(currArr[:], i + 1)
-                        currArr.pop()
+                    if isPalindrome(s[currIndex:i + 1]):
+                        backtrack(currArr[:] + [s[currIndex:i + 1]], i + 1)
 
         backtrack([], 0)
         return answer
