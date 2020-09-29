aed52fdcd790ba3f300fa649eaeab62ff5b63372
Herun Zhou <iamherunzhou@gmail.com>
2020-09-29 12:46:23 -0400

    redid lc 91

--- Medium/91. Decode Ways.py
+++ Medium/91. Decode Ways.py
@@ -4,22 +4,19 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        # dp[i] represents # of ways to decode message at length i
-        dp = [0] * (len(s) + 1)
+        if s[0] == "0":
+            return 0
+
+        # dp[i] = # of ways to decode at ith letter
+        dp = [0 for x in range(len(s) + 1)]
         dp[0] = 1
         dp[1] = 1
 
-        if s[0] == '0':
-            return 0
-
-        for i in range(2, len(dp)):
-            oneDigit = int(s[i - 1: i])
-            twoDigit = int(s[i - 2: i])
-
-            if 0 < oneDigit <= 9:
+        # 226
+        for i in range(2, len(s) + 1):
+            if 0 < int(s[i - 1: i]) <= 9:
                 dp[i] += dp[i - 1]
-
-            if 10 <= twoDigit <= 26:
+            if 10 <= int(s[i - 2: i]) <= 26:
                 dp[i] += dp[i - 2]
 
         return dp[-1]
