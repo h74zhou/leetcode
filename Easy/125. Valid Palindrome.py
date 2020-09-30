f369924d164a4d1393781abb4b067cf3bc92a235
Herun Zhou <iamherunzhou@gmail.com>
2020-09-29 22:30:47 -0400

    redid lc 125

--- Easy/125. Valid Palindrome.py
+++ Easy/125. Valid Palindrome.py
@@ -4,17 +4,15 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        i = 0
-        j = len(s) - 1
-
-        while i <= j:
+        i, j = 0, len(s) - 1
+        while i < j:
             if not s[i].isalnum():
                 i += 1
             elif not s[j].isalnum():
                 j -= 1
-            elif s[i].lower() != s[j].lower():
-                return False
             else:
+                if s[i].lower() != s[j].lower():
+                    return False
                 i += 1
                 j -= 1
 
