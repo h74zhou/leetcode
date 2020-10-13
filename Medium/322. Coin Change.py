05c1775aca5fe27e4e8484e8533e155fdf3cc518
Herun Zhou <iamherunzhou@gmail.com>
2020-10-13 13:31:35 -0400

    redid lc 322

--- Medium/322. Coin Change.py
+++ Medium/322. Coin Change.py
@@ -5,9 +5,12 @@ class Solution(object):
         :type amount: int
         :rtype: int
         """
-        dp = [float('inf')] * (amount + 1)
+        dp = [amount + 1 for x in range(amount + 1)]
         dp[0] = 0
-        for coin in coins:
-            for i in range(coin, amount + 1):
-                dp[i] = min(dp[i], 1 + dp[i - coin])
-        return dp[amount] if dp[amount] != float('inf') else -1
+
+        for i in range(1, amount + 1):
+            for coin in coins:
+                if i - coin >= 0:
+                    dp[i] = min(dp[i], 1 + dp[i - coin])
+
+        return dp[-1] if dp[-1] != amount + 1 else -1
