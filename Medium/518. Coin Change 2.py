a1f2c433153a40154623be0e671b86f7515c589d
Herun Zhou <iamherunzhou@gmail.com>
2020-10-14 21:16:50 -0400

    redid lc 518

--- Medium/518. Coin Change 2.py
+++ Medium/518. Coin Change 2.py
@@ -5,11 +5,12 @@ class Solution(object):
         :type coins: List[int]
         :rtype: int
         """
-        dp = [0] * (amount + 1)
+        dp = [0 for x in range(amount + 1)]
         dp[0] = 1
 
         for coin in coins:
-            for i in range(coin, amount + 1):
-                dp[i] += dp[i - coin]
+            for i in range(1, amount + 1):
+                if i >= coin:
+                    dp[i] += dp[i - coin]
 
-        return dp[amount]
+        return dp[-1]
