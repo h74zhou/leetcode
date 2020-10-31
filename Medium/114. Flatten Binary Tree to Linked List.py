a07df081547a111ebf5ff4ccad063f3910c9a5e4
Herun Zhou <iamherunzhou@gmail.com>
2020-10-31 16:59:10 -0400

    redid lc 114 iteratively

--- Medium/114. Flatten Binary Tree to Linked List.py
+++ Medium/114. Flatten Binary Tree to Linked List.py
@@ -5,19 +5,24 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
-    prev = None
-
     def flatten(self, root):
         """
         :type root: TreeNode
         :rtype: None Do not return anything, modify root in-place instead.
         """
-
         if root is None:
-            return
+            return root
 
-        self.flatten(root.right)
-        self.flatten(root.left)
-        root.right = self.prev
-        root.left = None
-        self.prev = root
+        curr = root
+
+        while curr:
+            if curr.left:
+                # if there's a left node, find its max
+                leftMax = curr.left
+                while leftMax.right:
+                    leftMax = leftMax.right
+                leftMax.right = curr.right
+                curr.right = curr.left
+                curr.left = None
+            curr = curr.right
+
