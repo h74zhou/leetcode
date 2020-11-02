fab95b130d49aeabf93ecff40a8a65c370891606
Herun Zhou <iamherunzhou@gmail.com>
2020-11-02 15:58:35 -0500

    finished lc 718

--- Medium/718. Maximum Length of Repeated Subarray.py
+++ Medium/718. Maximum Length of Repeated Subarray.py
@@ -5,3 +5,15 @@ class Solution(object):
         :type B: List[int]
         :rtype: int
         """
+        currMax = 0
+        dp = [[0 for x in range(len(A) + 1)] for y in range(len(B) + 1)]
+
+        for i in range(1, len(A) + 1):
+            for j in range(1, len(B) + 1):
+                if A[i - 1] == B[j - 1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                    currMax = max(dp[i][j], currMax)
+                else:
+                    dp[i][j] = 0
+
+        return currMax
