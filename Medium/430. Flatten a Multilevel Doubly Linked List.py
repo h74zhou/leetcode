b47a0ad8bc8f6cb23fcf11b202b97bd6ec6a250e
Herun Zhou <iamherunzhou@gmail.com>
2020-11-02 16:06:41 -0500

    redid lc 430

--- Medium/430. Flatten a Multilevel Doubly Linked List.py
+++ Medium/430. Flatten a Multilevel Doubly Linked List.py
@@ -14,28 +14,33 @@ class Solution(object):
         :type head: Node
         :rtype: Node
         """
+        if head is None:
+            return head
+
+        # create temp for curr and the next node
         curr = head
 
         while curr:
             if curr.child:
-                # save the right node
                 right = curr.next
 
-                # the front node of the i + 1 level, connect with i level
+                # recursively process children
                 curr.next = self.flatten(curr.child)
                 curr.next.prev = curr
                 curr.child = None
 
-                # traverse the i + 1 level to the end
+                # loop to the end
                 while curr.next:
                     curr = curr.next
 
-                # once you reach the end of the i + 1 level, connect it back with the original next
+                # connect the original next
                 if right:
                     curr.next = right
                     curr.next.prev = curr
+                    curr.child = None
 
             curr = curr.next
 
         return head
+
 
