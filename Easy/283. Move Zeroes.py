12d08cf00795380e1ae07fa9ebf979377615f2a4
Herun Zhou <iamherunzhou@gmail.com>
2020-11-02 20:06:20 -0500

    redid lc 283

--- Easy/283. Move Zeroes.py
+++ Easy/283. Move Zeroes.py
@@ -4,8 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        slow = 0  # slow represents non-zeros
+        lastNonZeroIndex = 0
         for i in range(len(nums)):
             if nums[i] != 0:
-                nums[slow], nums[i] = nums[i], nums[slow]
-                slow += 1
+                nums[i], nums[lastNonZeroIndex] = nums[lastNonZeroIndex], nums[i]
+                lastNonZeroIndex += 1
