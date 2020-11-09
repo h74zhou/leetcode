3625f916331708a6564c01cf3bfd7821f4250673
Herun Zhou <iamherunzhou@gmail.com>
2020-11-09 15:00:08 -0500

    redid lc 200

--- Medium/200. Number of Islands.py
+++ Medium/200. Number of Islands.py
@@ -4,21 +4,20 @@ class Solution(object):
         :type grid: List[List[str]]
         :rtype: int
         """
-
-        def dfs(grid, i, j):
+        def dfs(i, j):
             if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] == "0":
                 return
             grid[i][j] = "0"
-            dfs(grid, i + 1, j)
-            dfs(grid, i - 1, j)
-            dfs(grid, i, j + 1)
-            dfs(grid, i, j - 1)
+            dfs(i + 1, j)
+            dfs(i - 1, j)
+            dfs(i, j + 1)
+            dfs(i, j - 1)
 
         count = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == "1":
-                    dfs(grid, i, j)
+                    dfs(i, j)
                     count += 1
 
         return count
