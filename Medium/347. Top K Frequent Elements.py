e45ebfeee7ceca4bbdd2e49f13191bf01f4839e4
Herun Zhou <iamherunzhou@gmail.com>
2020-11-09 20:06:30 -0500

    redid lc 347

--- Medium/347. Top K Frequent Elements.py
+++ Medium/347. Top K Frequent Elements.py
@@ -1,6 +1,5 @@
 import heapq
 
-
 class Solution(object):
     def topKFrequent(self, nums, k):
         """
@@ -8,19 +7,28 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        d = {}
-        heap = []
+        # bucket[i] represent an array of elements with frequency of i
+        bucket = [None for x in range(len(nums) + 1)]
+        freq = {}
         answer = []
-        for i in nums:
-            if i in d:
-                d[i] += 1
+
+        for num in nums:
+            freq[num] = freq.get(num, 0) + 1
+
+        for key, freqAmt in freq.iteritems():
+            if bucket[freqAmt] is None:
+                bucket[freqAmt] = [key]
             else:
-                d[i] = 1
+                bucket[freqAmt].append(key)
 
-        for key, value in d.iteritems():
-            heapq.heappush(heap, (-value, key))
-
-        for i in range(k):
-            answer.append(heapq.heappop(heap)[1])
+        curr, count = len(bucket) - 1, 0
+        while curr >= 0 and count < k:
+            if bucket[curr] is not None:
+                for num in bucket[curr]:
+                    answer.append(num)
+                    count += 1
+            curr -= 1
 
         return answer
+
+
