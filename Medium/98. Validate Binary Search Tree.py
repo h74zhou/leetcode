7d3ef98fa27f213eadcf102851e148439678d28d
Herun Zhou <iamherunzhou@gmail.com>
2020-12-17 16:07:53 -0500

    redid lc 98

--- Medium/98. Validate Binary Search Tree.py
+++ Medium/98. Validate Binary Search Tree.py
@@ -10,54 +10,16 @@ class Solution(object):
         :type root: TreeNode
         :rtype: bool
         """
-        def helper(root, left, right):
-            if root is None:
+        def helper(node, leftVal, rightVal):
+            if node is None:
                 return True
-
-            if root.val <= left or root.val >= right:
+            if leftVal >= node.val or rightVal <= node.val:
                 return False
-
-            if not helper(root.left, left, root.val):
+            if not helper(node.left, leftVal, node.val):
                 return False
-
-            if not helper(root.right, root.val, right):
+            if not helper(node.right, node.val, rightVal):
                 return False
-
             return True
 
         return helper(root, float('-inf'), float('inf'))
 
-
-# Iterative Solution
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution(object):
-    def isValidBST(self, root):
-        """
-        :type root: TreeNode
-        :rtype: bool
-        """
-        if not root:
-            return True
-
-        stack = []
-        stack.append((root, (float('-inf'), float('inf'))))
-        while stack:
-            item = stack.pop()
-            node = item[0]
-            left = item[1][0]
-            right = item[1][1]
-
-            if node is None:
-                continue
-            elif node.val <= left or node.val >= right:
-                return False
-            else:
-                stack.append((node.left, (left, node.val)))
-                stack.append((node.right, (node.val, right)))
-
-        return True
