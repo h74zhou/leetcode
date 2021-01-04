a91e6b719bc60c1c577957f6be32907e263c9858
Herun Zhou <iamherunzhou@gmail.com>
2021-01-04 10:53:04 -0500

    redid lc 77

--- Medium/77. Combinations.py
+++ Medium/77. Combinations.py
@@ -8,11 +8,11 @@ class Solution(object):
         answer = []
 
         def backtrack(currArr, currIndex):
-            if len(currArr) >= k:
+            if len(currArr) == k:
                 answer.append(currArr[:])
             else:
                 for i in range(currIndex, n + 1):
-                    backtrack(currArr + [i], i + 1)
+                    backtrack(currArr[:] + [i], i + 1)
 
         backtrack([], 1)
         return answer
