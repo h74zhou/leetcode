ff510458d440fb3668ad2ea1996ada9d5c855665
Herun Zhou <iamherunzhou@gmail.com>
2021-01-09 22:34:19 -0500

    finished lc 1662 with pointers

--- Easy/1662. Check If Two String Arrays are Equivalent.py
+++ Easy/1662. Check If Two String Arrays are Equivalent.py
@@ -5,11 +5,22 @@ class Solution(object):
         :type word2: List[str]
         :rtype: bool
         """
-        first, second = "", ""
+        wordPointer1, wordPointer2 = 0, 0
+        letterPointer1, letterPointer2 = 0, 0
 
-        for word in word1:
-            first += word
-        for word in word2:
-            second += word
+        while wordPointer1 < len(word1) and wordPointer2 < len(word2):
+            if word1[wordPointer1][letterPointer1] != word2[wordPointer2][letterPointer2]:
+                return False
 
-        return first == second
+            letterPointer1 += 1
+            letterPointer2 += 1
+
+            if letterPointer1 >= len(word1[wordPointer1]):
+                letterPointer1 = 0
+                wordPointer1 += 1
+
+            if letterPointer2 >= len(word2[wordPointer2]):
+                letterPointer2 = 0
+                wordPointer2 += 1
+
+        return True if wordPointer1 >= len(word1) and wordPointer2 >= len(word2) else False
