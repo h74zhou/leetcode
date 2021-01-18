4aec4a2e5b2b795f206fcde0107704e50a590503
Herun Zhou <iamherunzhou@gmail.com>
2021-01-17 23:19:27 -0500

    finished lc 1641

---
+++ Medium/1641. Count Sorted Vowel Strings.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def countVowelStrings(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        a, e, i, o, u = 1, 1, 1, 1, 1
+
+        for x in range(n - 1):
+            a = a + e + i + o + u
+            e = e + i + o + u
+            i = i + o + u
+            o = o + u
+
+        return a + e + i + o + u
