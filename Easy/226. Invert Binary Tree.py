2b2240927b628877cc57a711e2929fab86ea6d90
Herun Zhou <iamherunzhou@gmail.com>
2021-02-17 10:05:43 -0500

    redid lc 226

--- Easy/226. Invert Binary Tree.py
+++ Easy/226. Invert Binary Tree.py
@@ -11,30 +11,11 @@ class Solution(object):
         :rtype: TreeNode
         """
         if root is None:
-            return root
+            return None
 
-        root.left, root.right = root.right, root.left
+        temp = root.left
+        root.left = root.right
+        root.right = temp
         self.invertTree(root.left)
         self.invertTree(root.right)
-        return root
-
-
-# Iterative Solution
-class Solution(object):
-    def invertTree(self, root):
-        """
-        :type root: TreeNode
-        :rtype: TreeNode
-        """
-        if root is None:
-            return root
-
-        stack = [root]
-        while stack:
-            node = stack.pop()
-            if node:
-                node.left, node.right = node.right, node.left
-                stack.append(node.left)
-                stack.append(node.right)
-
         return root
